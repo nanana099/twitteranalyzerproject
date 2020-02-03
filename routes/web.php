49b52d346614a80b85417f5d8a71559971494835
アキナミ <>
2020-02-03 20:32:24 +0900

    ADD:当アプリようにルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// ログインが不要なルート
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('mypage.monitor');
+    } else {
+        return view('welcome');
+    }
+});
+
+// ログインが必要なルート
+Route::middleware(['auth'])->group(function () {
+    // TwitterAPIのコールバック
+    Route::get('account/callback', 'TwitterController@callback');  
+    // ツイートデータ読み込み画面
+    Route::get('registtweet/show', 'RestTweetController@show');  
+    // ツイートデータ読み込み画面からの更新
+    Route::post('registtweet/post', 'RestTweetController@upsert');  
+    // ツイート一覧画面
+    Route::get('tweets/show', 'TweetsController@show');  
+    // 単語解析画面
+    Route::get('words/show', 'WordsController@show');  
+});
