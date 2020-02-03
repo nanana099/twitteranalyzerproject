767f5ed807217042962bdce5ed959926bdda148a
アキナミ <>
2020-02-03 19:55:47 +0900

    ADD:scss,jsをビルドする設定やパッケージを追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig({
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        enforce: "pre",
+        loader: "import-glob-loader"
+      }
+    ]
+  }
+})
+.js('resources/js/app.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css');
