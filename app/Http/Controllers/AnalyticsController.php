2c20b76f6cc76b6ecb3678bc3a6218bf04fa1bdd
アキナミ <>
2020-02-14 21:54:54 +0900

    ADD：必要な画面を表示するためのルートを設定

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WordAnalyzerController extends Controller
+class AnalyticsController extends Controller
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,15 @@
 */
 
 // ログインが不要なルート
+
+// 認証系
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function () {
     if (Auth::check()) {
-        return redirect()->route('mypage.monitor');
+        return redirect()->route('registtweet.show');
     } else {
         return view('welcome');
     }
@@ -26,14 +28,29 @@ Route::get('/', function () {
 
 // ログインが必要なルート
 Route::middleware(['auth'])->group(function () {
-    // TwitterAPIのコールバック
-    Route::get('account/callback', 'TwitterAccountController@callback');  
-    // ツイートデータ読み込み画面
-    Route::get('registtweet/show', 'RegistTweetController@show');  
-    // ツイートデータ読み込み画面からの更新
-    Route::post('registtweet/post', 'RegistTweetController@upsert');  
-    // ツイート一覧画面
-    Route::get('tweets/show', 'TweetListController@show');  
-    // 単語解析画面
-    Route::get('words/show', 'WordAnalyzerController@show');  
+    // １．Twitterアカウントの登録
+    Route::get('twitteraccount/callback', 'TwitterAccountController@callback'); // TwitterAPIからのコールバック
+    // ２．ツイートデータ読み込み画面
+    Route::get('registtweet/show', 'RegistTweetController@show')->name('registtweet.show'); // ツイートデータ登録画面を表示
+    Route::post('registtweet/post', 'RegistTweetController@upsert');            // ツイートデータを更新
+    // ３．過去のツイート一覧画面
+    Route::get('tweetlist/show', 'TweetListController@show');                   //過去ツイート一覧を表示
+    // ４．分析画面
+    Route::get('analytics/show', 'AnalyticsController@show');                   // 分析画面表示 
+    // ５．設定
+    Route::get('setting/account/show', 'SettingController@showAccount');        // アカウント設定
+    Route::get('setting/screen/show', 'SettingController@showScreen');          // 画面設定
+    Route::get('setting/twitteraccount/show', 'SettingController@showTwitterAccount');  // ツイッターアカウント設定
+    Route::get('setting/notify/show', 'SettingController@showNotify');          // 通知設定
+    Route::get('setting/plan/show', 'SettingController@showPlan');              // プラン設定
+    // ６．連絡
+    Route::get('contact/inquirey/show', 'ContactController@showInquiry');       // お問い合わせ
+    Route::get('contact/opinion/show', 'ContactController@showOpiniton');       // ご意見・ご要望
+    // ７．ヘルプ関連
+    Route::get('help/qa/show', 'HelpController@showQA');                        // QA画面
+    Route::get('help/example/show', 'HelpController@showExample');              // 使い方の例
+    // ８．プレーンテキスト
+    Route::get('plane/rule/show', 'PlaneTextController@showRule');              // 利用規約
+    Route::get('plane/tokusho/show', 'PlaneTextController@showTokusho');        // 特商
+    Route::get('plane/privacy/show', 'PlaneTextController@showPrivacy');        // プライバシーポリシー
 });
