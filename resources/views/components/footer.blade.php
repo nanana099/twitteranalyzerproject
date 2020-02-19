19d303aa2430e1c52cc20615baec074a2d458f9f
アキナミ <>
2020-02-19 21:20:41 +0900

    ADD:フッターにコピーライト、各種リンクを設置

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,13 @@
 <footer id="footer">
     <div class="c-site-width">
-        contents
+        <div class="p-footer__links">
+            <a class="p-footer__link" href="">お問い合わせ</a>
+            <a class="p-footer__link" href="">利用規約</a>
+            <a class="p-footer__link" href="">特定商取引法に基づく表示</a>
+            <a class="p-footer__link" href="">プライバシーポリシー</a>
+        </div>
+        <div class="c-copy">
+            Copyright © {{ config('app.name', 'つぶやきプランナー') }} All Rights Reserved.
+        </div>
     </div>
 </footer>
