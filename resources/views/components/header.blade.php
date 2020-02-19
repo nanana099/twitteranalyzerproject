49e71874bbc98d455a6748c39e6a963c2cf581de
アキナミ <>
2020-02-19 13:24:13 +0900

    ADD:ヘッダー表示をログイン状態ごとに切り替え

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,7 +10,7 @@ $login = 1;
         </div>
         <nav class="">
             <ul class="c-gnav__list">
-                @if($login)
+                @if(Auth::check())
                 <li class="c-gnav__item"><a href="{{route('tweetlist.show')}}" class="c-gnav__link">ツイート <i
                             class="fas fa-list-ul"></i></a></li>
                 <li class="c-gnav__item"><a href="{{route('analytics.show')}}" class="c-gnav__link">分析 <i class="fas fa-chart-bar"></i></a></li>
@@ -24,8 +24,8 @@ $login = 1;
                     </a></li>
                 <li class="c-gnav__item"><a href="" class=""><i class="fas fa-cog p-header__setting-icon"></i></a></li>
                 @else
-                <li class="c-gnav__item"><a href="" class="c-btn c-btn--primary c-btn--square">無料で使ってみる</a></li>
-                <li class="c-gnav__item"><a href="" class="c-btn c-btn--square">ログイン</a></li>
+                <li class="c-gnav__item"><a href="{{ route('register') }}" class="c-btn c-btn--primary c-btn--square">無料で使ってみる</a></li>
+                <li class="c-gnav__item"><a href="{{ route('login') }}" class="c-btn c-btn--square">ログイン</a></li>
                 @endif
             </ul>
         </nav>
