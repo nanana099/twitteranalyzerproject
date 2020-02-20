1d7780826155c18747aa428a02831cb3a9a85f8a
アキナミ <>
2020-02-20 13:47:16 +0900

    FIX:フッターメニューの順番変更

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,8 @@
         <div class="p-footer__links">
             <a class="p-footer__link" href="">お問い合わせ</a>
             <a class="p-footer__link" href="">利用規約</a>
-            <a class="p-footer__link" href="">特定商取引法に基づく表記</a>
             <a class="p-footer__link" href="">プライバシーポリシー</a>
+            <a class="p-footer__link" href="">特定商取引法に基づく表記</a>
         </div>
         <div class="c-copy">
             Copyright © {{ config('app.name', 'つぶやきプランナー') }} All Rights Reserved.
